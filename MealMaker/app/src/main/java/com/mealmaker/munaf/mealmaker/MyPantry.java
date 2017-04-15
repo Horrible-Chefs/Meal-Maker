4ea45fa58e6bb902af6948b5e92bd349c62cd6d7
maqzi <munaf.qazi@gmail.com>
2017-04-14 22:14:06 -0400

    pantryDeletable+initialUItesting

--- MealMaker/app/src/main/java/com/mealmaker/munaf/mealmaker/MyPantry.java
+++ MealMaker/app/src/main/java/com/mealmaker/munaf/mealmaker/MyPantry.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -14,8 +19,11 @@ public class MyPantry extends AppCompatActivity{
 
     private final static String TAG = "MyPantry";
     private DBHandler db;
-    private TextView tv;
-    private ArrayList<PantryItem> list;
+//    private TextView tv;
+    private ListView lv;
+    private ArrayList<String> list = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private ArrayList<PantryItem> allItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +31,28 @@ public class MyPantry extends AppCompatActivity{
         setContentView(R.layout.mypantrylayout);
 
         db = new DBHandler(this);
-        tv = (TextView) findViewById(R.id.tv_pantrylist);
-        Button addMore = (Button) findViewById(R.id.btn_addmore);
-        Button imHungry = (Button) findViewById(R.id.btn_imhungry);
-        Button reset = (Button) findViewById(R.id.btn_reset);
+//        tv = (TextView) findViewById(R.id.tv_pantrylist);
+        ImageButton addMore = (ImageButton) findViewById(R.id.btn_addmore);
+        ImageButton imHungry = (ImageButton) findViewById(R.id.btn_imhungry);
+        ImageButton reset = (ImageButton) findViewById(R.id.btn_reset);
 
         //Fill DB for testing
         Log.i(TAG,"Inserting/Deleting items");
         Log.i(TAG,"Inserted/Deleted items");
 
         refresh();
+        registerClickCallback();
+
 
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 db.deleteAll();
+                list.clear();
                 refresh();
+                String message = "pushed everything down the drain!";
+                Toast.makeText(MyPantry.this,message,Toast.LENGTH_LONG).show();
+
             }
         });
 
@@ -57,7 +71,7 @@ public class MyPantry extends AppCompatActivity{
             public void onClick(View v) {
                 Log.i(TAG,"Should access im hungry stub");
                 Intent intent_imhungry = new Intent(getApplicationContext(), ImHungry.class);
-                intent_imhungry.putExtra("pantry_data", list);
+                intent_imhungry.putExtra("pantry_data", allItems);
                 startActivity(intent_imhungry);
             }
 
@@ -71,16 +85,39 @@ public class MyPantry extends AppCompatActivity{
             refresh();
         }
     }
+    private void registerClickCallback(){
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView tv2 = (TextView) view;
+                String message = tv2.getText().toString()+" deleted.";
+                Toast.makeText(MyPantry.this,message,Toast.LENGTH_LONG).show();
+                db.deleteItem (((TextView) view).getText().toString());
+                list.remove(tv2.getText().toString());
+                refresh();
+                Log.i(TAG,"Deleted something. listview updated");
+            }
+        });
+    }
 
     private void refresh(){
         Log.i("MyPantry: ", "Reading all items..");
-        tv.setText("");
-        list = (ArrayList) db.getAllItems();
-        if(list.size()==0){
-            tv.setText("Nothing in your Pantry!");
+//        tv.setText("");
+        allItems = (ArrayList) db.getAllItems();
+        for (PantryItem item : allItems){
+            if(!list.contains(item.getName())){
+                list.add(item.getName());
+            }
         }
-        for (PantryItem item : list) {
-            tv.append(item.getName()+"\n");
+        if(list.isEmpty()){
+//            tv.setText("Nothing in your Pantry!");
         }
+        adapter = new ArrayAdapter<>(
+                this,
+                R.layout.each_item,
+                list);
+
+        lv = (ListView) findViewById(R.id.listview_pantry);
+        lv.setAdapter(adapter);
     }
 }
